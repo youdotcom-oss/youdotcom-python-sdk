07aaee8bd991f42602823fff1cfa2449cf24df6b
Tyler Eastman <tyler@you.com>
2025-11-17 17:13:16 -0800

    Update version to 0.0.2

--- src/youdotcom/_version.py
+++ src/youdotcom/_version.py
@@ -3,10 +3,10 @@
 import importlib.metadata
 
 __title__: str = "youdotcom"
-__version__: str = "0.0.1"
-__openapi_doc_version__: str = "0.0.1"
+__version__: str = "0.0.2"
+__openapi_doc_version__: str = "0.0.2"
 __gen_version__: str = "2.755.9"
-__user_agent__: str = "speakeasy-sdk/python 0.0.1 2.755.9 0.0.1 youdotcom"
+__user_agent__: str = "youdotcom-python-sdk/0.0.2"
 
 try:
     if __package__ is not None:
