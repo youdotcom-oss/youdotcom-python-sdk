3d1d428b65226d1ceeb17ef88561c05de0c668f5
Tyler Eastman <tyler@you.com>
2025-11-19 10:21:49 -0800

    Update version to 1.3.0 for PyPi compatibility

--- src/youdotcom/_version.py
+++ src/youdotcom/_version.py
@@ -1,10 +1,10 @@
 import importlib.metadata
 
 __title__: str = "youdotcom"
-__version__: str = "1.0.0"
+__version__: str = "1.3.0"
 __openapi_doc_version__: str = "1.0.0"
 __gen_version__: str = "2.755.9"
-__user_agent__: str = "youdotcom-python-sdk/1.0.0"
+__user_agent__: str = "youdotcom-python-sdk/1.3.0"
 
 try:
     if __package__ is not None:
